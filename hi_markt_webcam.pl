e512289f07ad24c6be82da7a7bd722a4201cc063
Sascha Fiebig <sascha.fiebig@posteo.net>
2023-06-06 00:28:41 +0200

    simplify code

--- hi_markt_webcam.pl
+++ hi_markt_webcam.pl
@@ -12,43 +12,16 @@ my $s_suffix           = '.jpg';
 my $i_pause_in_seconds = 60;
 
 
-sub date_timestamp{
-    my $i_current_unix_time = 7200+time();
-    # extract year, month, day, hour, min, sec
-    #my $s_year   = strftime '%Y', gmtime( $i_current_unix_time ); 
-    #my $s_month  = strftime '%m', gmtime( $i_current_unix_time ); 
-    #my $s_day    = strftime '%d', gmtime( $i_current_unix_time ); 
-    #my $s_hour   = strftime '%H', gmtime( $i_current_unix_time ); 
-    #my $s_minute = strftime '%M', gmtime( $i_current_unix_time ); 
-    #my $s_second = strftime '%S', gmtime( $i_current_unix_time ); 
-    #my $s_date_timestamp = "$s_year-$s_month-$s_day" . '_' . "$s_hour:$s_minute:$s_second";
-
-    my $s_test = strftime '%Y-%m-%d_%H:%M:%S', gmtime( $i_current_unix_time ); 
-    
-    
-    #return $s_date_timestamp;
-    return $s_test;
-}
-
-
-if(1){
-    my $s_test = date_timestamp();
-    print( "\n\n$s_test\n\n" );
-}
-
-
 print( RED, "----------[ STARTING DOWNLOAD ]----------\n\n", RESET );
 
-while(0){
+while(1){
     # generate timestamp
-    my $s_timestamp    = date_timestamp();
-    #my $i_current_unix_time = time();
-    #my $s_timestamp = strftime '%Y-%m-%d_%H:%M:%S', gmtime( $i_current_unix_time );
-
+    my $i_current_unix_time = 7200+time();
+    my $s_timestamp    = strftime '%Y-%m-%d_%H%M%S', gmtime( $i_current_unix_time );
     # assemble filename 
     my $s_filename     = $s_prefix . $s_timestamp . $s_suffix;
     # assemble wget command 
-    my $s_wget_command = 'wget -O ' . $s_filename . " --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'" . $s_download_url;
+    my $s_wget_command = 'wget -O ' . $s_filename . " --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0' " . $s_download_url;
 
     # print progress 
     print( RED, 'Download file: ' . $s_filename );
@@ -62,3 +35,4 @@ while(0){
 } 
 
 print( RED, "----------[ DOWNLOAD COMPLETE ]----------\n", RESET );
+
