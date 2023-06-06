f9c0cfd5d3f73b2c05868755556af12b5adc1278
Sascha Fiebig <sascha.fiebig@posteo.net>
2023-06-06 07:37:22 +0200

    improve timestamp
    *refactor codestyle

--- hi_markt_webcam.pl
+++ hi_markt_webcam.pl
@@ -6,32 +6,30 @@ use diagnostics;
 use POSIX qw(strftime);
 use Term::ANSIColor qw(:constants);
 
+
 my $s_download_url     = 'http://rathaus-hildesheim.de/webcam/webcam.jpg';
 my $s_prefix           = 'hildesheim_marktplatz_';
 my $s_suffix           = '.jpg';
 my $i_pause_in_seconds = 60;
 
-
 print( RED, "----------[ STARTING DOWNLOAD ]----------\n\n", RESET );
 
 while(1){
     # generate timestamp
-    my $i_current_unix_time = 7200+time();
-    my $s_timestamp    = strftime '%Y-%m-%d_%H%M%S', gmtime( $i_current_unix_time );
+    my $s_timestamp    = strftime '%Y-%m-%d_%H%M%S', localtime();
     # assemble filename 
     my $s_filename     = $s_prefix . $s_timestamp . $s_suffix;
     # assemble wget command 
     my $s_wget_command = 'wget -O ' . $s_filename . " --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0' " . $s_download_url;
 
     # print progress 
-    print( RED, 'Download file: ' . $s_filename );
-    print( 'From: ' . $s_download_url, RESET    );
+    print( RED, 'Download file: ' . $s_filename . "\n" );
+    print( 'From:          ' . $s_download_url . "\n", RESET );
 
     # execute wget command 
     my $EXECUTE = `$s_wget_command`;
     sleep( $i_pause_in_seconds );
     print( "\n"                );
-
 } 
 
 print( RED, "----------[ DOWNLOAD COMPLETE ]----------\n", RESET );
