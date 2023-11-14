df6ab863acef267796981adb2838440e3f531529
Sascha Fiebig <sascha.fiebig@posteo.net>
2023-11-14 15:16:55 +0100

    add filter to remove non test files

--- run_all_tests.pl
+++ run_all_tests.pl
@@ -23,18 +23,23 @@ sub start_test {
 
 
 # get test files from sub directories 
-system( 'tree -fi | grep -P \\.t$ > test_files.csv' );
+system( 'tree -fi | grep -P \\.t$ > test_files.txt' );
 
-my @a_test_files;
+my @a_files;
 # open file for reading
-open( my $fh, '<', 'test_files.csv' ) or die( "Error: Can't open file: $!" );
+open( my $fh, '<', 'test_files.txt' ) or die( "Error: Can't open file: $!" );
     while( my $s_line = <$fh> ) {
-        push @a_test_files, $s_line;
+        push @a_files, $s_line;
     }
     close( $fh ) or die("Error: Can't close file: $!");
 
 # delete file 
-unlink 'test_files.csv';
+unlink 'test_files.txt';
+
+# pop off any non '.t' entry from @a_test_files so we can change test_files.txt to .txt
+my @a_test_files = grep{/\.t$/} @a_files;
+
+
 
 # execute test files 
 foreach my $s_test (@a_test_files) {
