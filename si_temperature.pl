b340361c37e5df5c2aafb31a2b3eb401dd4ce797
Sascha Fiebig <sascha.fiebig@posteo.net>
2024-09-08 12:02:21 +0200

    revamp si_temperature app

--- si_temperature.pl
+++ si_temperature.pl
@@ -6,83 +6,88 @@ use warnings;
 
 use lib '.';
 use lib '../lib';
+
 use Modules::Temperature;
 use Term::ANSIColor qw(:constants);
+
+
+sub from_celsius {
+    my ($o_temperature, $f_temperature) = @_;
+
+    my $f_kelvin     = $o_temperature->celsius_to_kelvin($f_temperature);
+    my $f_celsius    = $o_temperature->kelvin_to_celsius($f_kelvin);
+    my $f_fahrenheit = $o_temperature->kelvin_to_fahrenheit($f_kelvin);
+
+    print(RED, "\t$f_celsius °C\n", RESET);
+    print("\t$f_fahrenheit °F\n");
+    print("\t$f_kelvin K\n");
+
+    return 1;
+}
+
+sub from_fahrenheit {
+    my ($o_temperature, $f_temperature) = @_;
+
+    my $f_kelvin     = $o_temperature->fahrenheit_to_kelvin($f_temperature);
+    my $f_celsius    = $o_temperature->kelvin_to_celsius($f_kelvin);
+    my $f_fahrenheit = $o_temperature->kelvin_to_fahrenheit($f_kelvin);
+
+    print("\t$f_celsius °C\n");
+    print(RED, "\t$f_fahrenheit °F\n", RESET);
+    print("\t$f_kelvin K\n");
+
+    return 1;
+}
+
+sub from_kelvin {
+    my ($o_temperature, $f_temperature) = @_;
+
+    my $f_kelvin     = $f_temperature;
+    my $f_celsius    = $o_temperature->kelvin_to_celsius($f_kelvin);
+    my $f_fahrenheit = $o_temperature->kelvin_to_fahrenheit($f_kelvin);
+
+    print("\t$f_celsius °C\n");
+    print("\t$f_fahrenheit °F\n");
+    print(RED, "\t$f_kelvin K\n", RESET);
+
+    return 1;
+}
 
 
 my $o_temperature = Modules::Temperature->new();
 
 while (1) {
     system('clear');
-    print("\n\n\nPlease enter the temperature you want to convert from: \n");
-    print("[1]: Celsius \n[2]: Fahrenheit \n[3]: Kelvin\n[q]: quit\n");
-    my $i_temp_from   = <STDIN>;
-    chomp($i_temp_from);
 
-    last unless($i_temp_from =~ /[1-3]/);
+    print("\n\n[C]: Celsius \n[F]: Fahrenheit \n[K]: Kelvin\n[q]: quit");
+    print("\nPlease enter the unit you want to convert from: ");
+    my $s_unit = <STDIN>;
+    chomp($s_unit);
+    $s_unit = lc($s_unit);
 
-    print("Please enter the temperature you want to convert to: \n");
-    print("[1]: Celsius \n[2]: Fahrenheit \n[3]: Kelvin\n");
-    my $i_temp_to     = <STDIN>;
-    chomp($i_temp_to);
-
-    print("Please enter the temperature: \n");
+    print("Please enter the temperature: ");
     my $f_temperature = <STDIN>;
     chomp($f_temperature);
 
-    my $s_from_temperature;
-    my $s_to_temperature;
-    my $f_result;
-
-    if ($i_temp_from eq 1) {
-        $s_from_temperature = 'Celsius';
-        if ($i_temp_to eq 2) {
-            $s_to_temperature = 'Fahrenheit';
-            $f_result = $o_temperature->kelvin_to_fahrenheit($o_temperature->celsius_to_kelvin($f_temperature)); 
-        }
-        elsif ($i_temp_to eq 3) {
-            $s_to_temperature = 'Kelvin';
-            $f_result = $o_temperature->celsius_to_kelvin($f_temperature); 
-        }
-        else {
-            die("Error: Invalide input!");
-        }
+    if (!($f_temperature =~ /\d/)) {
+        die(RED, "Error: \"$f_temperature\" is not a valide temperature!", RESET);
     }
-    elsif ($i_temp_from eq 2) {
-        $s_from_temperature = 'Fahrenheit';
-        if ($i_temp_to eq 1) {
-            $s_to_temperature = 'Celsius';
-            $f_result = $o_temperature->kelvin_to_celsius($o_temperature->fahrenheit_to_kelvin($f_temperature)); 
-        }
-        elsif ($i_temp_to eq 3) {
-            $s_to_temperature = 'Kelvin';
-            $f_result = $o_temperature->fahrenheit_to_kelvin($f_temperature); 
-        }
-        else {
-            die("Error: Invalide input!");
-        }
+    elsif ($s_unit eq 'c') {
+        from_celsius($o_temperature, $f_temperature);
     }
-    elsif ($i_temp_from eq 3) {
-        $s_from_temperature = 'Kelvin';
-        if ($i_temp_to eq 1) {
-            $s_to_temperature = 'Celsius';
-            $f_result = $o_temperature->kelvin_to_celsius($f_temperature); 
-        }
-        elsif ($i_temp_to eq 2) {
-            $s_to_temperature = 'Fahrenheit';
-            $f_result = $o_temperature->kelvin_to_fahrenheit($f_temperature); 
-        }
-        else {
-            die("Error: Invalide input!");
-        }
+    elsif ($s_unit eq 'f') {
+        from_fahrenheit($o_temperature, $f_temperature);
+    }
+    elsif ($s_unit eq 'k') {
+        from_kelvin($o_temperature, $f_temperature);
     }
     else {
-        die("Error: Invalide input!");
+        die(RED, "Error: \"$s_unit\" is not a valide temperature unit!", RESET);
     }
 
-    my $f_formattet_result = sprintf("%.2f", $f_result);
-    print(RED, "$f_temperature $s_from_temperature = $f_formattet_result $s_to_temperature\n\n\n", RESET);
-    print('Please press enter to continue.');
-    my $hold = <STDIN>;
+    print("\n\n\t-- Enter 'q' to quit --\n");
+    my $s_hold = <STDIN>;
+    chomp($s_hold);
+    last if $s_hold eq 'q';
 }
 
